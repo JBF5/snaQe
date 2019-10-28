86127e94acdc645444a95f1ab9cda84ed591606b
Jonas <JBF5@pct.edu>
2019-10-28 15:11:35 -0400

    Added the qBot script

---
+++ Assets/Scripts/qBot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class qBot
+{
+    private List<float[]> QStates;
+    private List<float[]> QActions;
+
+    private int delcaredActions;
+    private float[] initialState;
+    private int initialActionIndex;
+    private float[] outcomeState;
+    private float outcomeActionValue;
+
+    private float lr;
+    private float y;
+    private float SimInterval;
+
+    private bool firstIteration;
+    System.Random rn = new System.Random();
+
+    public qBot(int actions)
+    {
+        QStates = new List<float[]>();
+        QActions = new List<float[]>();
+        delcaredActions = actions;
+
+        lr = .8f;
+        y = .95f;
+
+        SimInterval = 1f;
+
+        firstIteration = true;
+    }
+
+
+    public static int ProcessSight(float[] sight, float reward)
+    {
+       
+
+
+
+        return 0;
+    }
+
+}
